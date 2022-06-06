d2f34e0f160d5033be5a7d017e1d6e0e699f1442
Pappouth <lammnslouis.rs@gmail.com>
2022-06-06 18:36:39 +0200

    rect sprite
    Ajout d'un attribut _rect à la class Sprite.

--- Sprite.cs
+++ Sprite.cs
@@ -10,12 +10,14 @@ namespace Envahisseurs_de_l_espace
         public Texture2D _texture;
         public Vector2 _position;
         public float _speed;
+        public Rectangle _rect;
 
         public Sprite(Texture2D SpriteTexture, float Speed, Vector2 Position)
         {
             _texture = SpriteTexture;
             _speed = Speed;
             _position = Position;
+            _rect = new Rectangle((int)this._position.X, (int)this._position.Y, this._texture.Width, this._texture.Height);
         }
 
         public virtual void Update(List<Sprite> sprites)
